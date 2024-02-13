633a140c6017ed6e9095de476e1e84dfea77c5e4
Amir Ali anwar <amiralianwar611@gmail.com>
2024-02-13 22:32:33 +0500

    setup the clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Logo from '@/assets/logo.svg'
 import LandingImg from '@/assets/main.svg'
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
@@ -11,6 +14,9 @@ export default function Home() {
         <div>
           <h1 className="captalize text-4xl md:text-7xl font-bold">Job <span className="text-primary">Tracking</span> app</h1>
           <p className="leading-loose max-w-md mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, in! Fugiat, quos? Vel tempora officiis, doloribus facere quod numquam autem nulla quisquam deserunt minus repudiandae perspiciatis ullam est dolorem assumenda!</p>
+          <Button asChild className='mt-4'>
+            <Link href='/add-job'>Get Started</Link>
+          </Button>
         </div>
         <Image src={LandingImg} alt="Landing" className="hidden lg:block" />
       </section>

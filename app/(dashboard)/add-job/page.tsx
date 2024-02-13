3d41b6550331d53c29be232c14a5c91cd045aacc
Amir Ali anwar <amiralianwar611@gmail.com>
2024-02-13 22:18:40 +0500

    working on the project assets

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>Add Jobs</div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
-import { Camera } from 'lucide-react'
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Logo from '@/assets/logo.svg'
+import LandingImg from '@/assets/main.svg'
 export default function Home() {
   return (
-    <div className='h-screen flex items-center justify-center'>
-
-      <Button>Default Button</Button>
-      <Button variant="outline" size="icon">
-        <Camera />
-      </Button>
-    </div>
+    <main>
+      <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6 '>
+        <Image src={Logo} alt="Logo image" />
+      </header>
+      <section className="max-w-6xl mx-auto px-4 sm:px-8 mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+        <div>
+          <h1 className="captalize text-4xl md:text-7xl font-bold">Job <span className="text-primary">Tracking</span> app</h1>
+          <p className="leading-loose max-w-md mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, in! Fugiat, quos? Vel tempora officiis, doloribus facere quod numquam autem nulla quisquam deserunt minus repudiandae perspiciatis ullam est dolorem assumenda!</p>
+        </div>
+        <Image src={LandingImg} alt="Landing" className="hidden lg:block" />
+      </section>
+    </main>
   );
 }
